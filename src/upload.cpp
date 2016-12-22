ba846f3f963bcf58cf7f14610f187b5f0cd85a39
Sigifredo Escobar Gómez <sigifredo89@gmail.com>
2016-12-21 23:23:26 -0500

    upload updated

--- src/upload.cpp
+++ src/upload.cpp
@@ -6,53 +6,54 @@
 
 int main()
 {
-    FILE *out;
+    FILE *fd;
     char *rawdata; // pointer for rawdata
     char *data; // will be an offset of rawdata after the two newlines
-    unsigned long length; // length of data to be read used for malloc
-    unsigned long writelength; // use for the length of the write
+    long length; // length of data to be read used for malloc
+    long writelength; // use for the length of the write
     char *pos; // used in the loop
 
-    printf("Content-type: text/html\n\n<HTML><PRE>"); // for debug output
+    std::printf("Content-type: text/plain\r\n\r\n"); // for debug output
     // Various bits of debug output are included - comment out for live
     // and replace with whatever output you'd like to send to the client
 
-    out = fopen("/var/www/test.jpg","wb+"); // open the output file
+    fd = fopen("/tmp/file.dat","wb+"); // open the output file
 
-    length=atol(getenv("CONTENT_LENGTH"));
-    writelength=length;
+    length = std::atol(getenv("CONTENT_LENGTH"));
+    writelength = length;
 
-    printf("Content Length: %u\n",length);
+    printf("Content Length: %l\n", length);
 
-    rawdata=malloc(length+1); // malloc required buffer
+    rawdata = new char[length+1]; // malloc required buffer
 
     fread(rawdata, length, 1, stdin); // read into buffer
 
     // now comes the loop, there are better ways but not that I can find quickly enough
-    for (pos=rawdata; pos<(rawdata+length-4); pos++)
+    for (pos = rawdata; pos < (rawdata+length-4); pos++)
     {
         writelength--; // decrement the write length
-        printf("%c %d\n",pos[0],pos[0]); // used for debug output (comment out for live)
+        printf("%c", pos[0]); // used for debug output (comment out for live)
+
         if ( (pos[0]==13) && (pos[1]==10) && (pos[2]==13) && (pos[3]==10) && ( (pos[4]<32)||(pos[4]>127) ) ) // pattern to find two double-newlines
         {
-            data=pos+4; // move data pointer forward 4 to start of actual data
-            pos=rawdata+length+2; // break loop
+            data = pos+4; // move data pointer forward 4 to start of actual data
             printf("Found\n"); // another debug line - comment out for live
             writelength-=3; // decrement writelength by three (done one already above for this loop)
+            break;
         }
     }
 
     printf("Writelength: %u\n",writelength); // yet another debug
 
     // write the data to the file
-    fwrite(data, 1, writelength, out);
+    fwrite(data, 1, writelength, fd);
 
     // close the file
-    fclose(out);
+    fclose(fd);
 
-    free(rawdata); // free memory
+    delete [] rawdata; // free memory
 
-    printf("Upload Complete"); // debug - comment out for live
+    std::printf("Upload Complete"); // debug - comment out for live
 
     return 0; // exit
 }
